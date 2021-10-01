801cc17f2d78be546dd439327cd9a90a88d16082
Reinaldo Assis <reinaldo.assis@leanonsystems.com>
2021-10-01 17:26:59 -0300

    links added

--- myfirstwebapp/src/main/resources/webapp/webapp.js
+++ myfirstwebapp/src/main/resources/webapp/webapp.js
@@ -8,7 +8,17 @@ router.get('/page/subpage', function() { return renderPage('Gone to sub page ind
 function renderPage(message) {
   var model = {
     title: 'Hello router',
-    message: message
+    message: message,
+    posts: {
+      teclado: 'para digitar',
+      mouse: 'para mexer',
+      cpu: 'para processar',
+      gpu: 'para processar tambem, mas diferente',
+      placamae: 'para juntar tudo',
+      ram: 'para guardar temporariamente coisas',
+      hd: 'para guardar coisas, mas lento',
+      ssd: 'para guardar coisas, mas rapido'
+    }
   };
   return  {
     body: thymeleaf.render(resolve('hello.html'), model)
